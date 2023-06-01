bd96a92b9df803f4cdb7a6a5c22567660fae15ce
LISA DAIGLE <lisadaigle@gmail.com>
2023-05-31 20:56:35 -0400

    Create Placeholder MainPage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { MainPage } from "./MainPage";
 
 function App() {
   return (
     <div>
       <Header />
+      <MainPage />
       <Footer />
     </div>
   );
